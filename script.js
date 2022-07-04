ec16fecb51d93e506bcc88f705371e9fc17095e5
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-04 14:19:37 +0100

    finished the last part for now
    -added the "playGame" function so it would play the game 5 times instead of 1.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-do {
-    playerSelection = prompt("please chose your play (rock, paper, scissors)", String)
-    playerSelection = playerSelection.toLowerCase()
-} while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
-computerSelection = computerPlay()
-result = playRound(playerSelection, computerSelection)
+playGame()
 
 function computerPlay() {
     const play=["rock", "paper", "scissors"]
     return play[Math.floor(Math.random() * 3)]
+}
+
+function playGame() {
+    for(let i=0; i < 5; i++) {
+
+        do {
+            playerSelection = prompt("please chose your play (rock, paper, scissors)", String)
+            playerSelection = playerSelection.toLowerCase()
+        } while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
+
+        computerSelection = computerPlay()
+        result = playRound(playerSelection, computerSelection)
+        
+        if(result == "you win!") {
+            console.log(result + " " + playerSelection + " beats " + computerSelection)
+        }else if(result == "you lose!") {
+            console.log(result + " " + playerSelection + " loses to " + computerSelection)
+        }else {
+            console.log(result + " " + playerSelection + " ties " + computerSelection)
+        }
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -38,12 +54,4 @@ function playRound(playerSelection, computerSelection) {
         }
     }
     return result
-}
-
-if(result == "you win!") {
-    console.log(result + " " + playerSelection + " beats " + computerSelection)
-}else if(result == "you lose!") {
-    console.log(result + " " + playerSelection + " loses to " + computerSelection)
-}else {
-    console.log(result + " " + playerSelection + " ties " + computerSelection)
 }
