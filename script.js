ce591de9d29cae117c33363dbf5699c111f07230
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-04 13:40:41 +0100

    added the "playRound" function so it is playable now

--- script.js
+++ script.js
@@ -3,8 +3,47 @@ do {
     playerSelection = playerSelection.toLowerCase()
 } while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
 computerSelection = computerPlay()
+result = playRound(playerSelection, computerSelection)
 
 function computerPlay() {
     const play=["rock", "paper", "scissors"]
     return play[Math.floor(Math.random() * 3)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result
+    if(playerSelection == "rock") {
+        if(computerSelection == "rock") {
+            result = "tie!"
+        }else if(computerSelection == "paper") {
+            result = "you lose!"
+        }else {
+            result = "you win!"
+        }
+    }else if(playerSelection == "paper") {
+        if(computerSelection == "rock") {
+            result = "you win!"
+        }else if(computerSelection == "paper") {
+            result = "tie!"
+        }else {
+            result = "you lose!"
+        }
+    }else {
+        if(computerSelection == "rock") {
+            result = "you lose!"
+        }else if(computerSelection == "paper") {
+            result = "you win!"
+        }else {
+            result = "tie!"
+        }
+    }
+    return result
+}
+
+if(result == "you win!") {
+    console.log(result + " " + playerSelection + " beats " + computerSelection)
+}else if(result == "you lose!") {
+    console.log(result + " " + playerSelection + " loses to " + computerSelection)
+}else {
+    console.log(result + " " + playerSelection + " ties " + computerSelection)
 }
