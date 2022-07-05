24a16bf45f891c9b6589f1935973269496afd81c
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-05 20:03:54 +0100

    added css and other stuff
    -gave the page simple design and structer
    -added buttons to decide how much score is required to win
    -added images to the buttons

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-let wins=0, losses=0;
+let wins=0, losses=0, n=5;
+
+const rnd1= document.querySelector(".rnd1")
+rnd1.addEventListener("click", () => {n=1; losses=0; wins=0})
+const rnd2= document.querySelector(".rnd2")
+rnd2.addEventListener("click", () => {n=5; losses=0; wins=0})
+const rnd3= document.querySelector(".rnd3")
+rnd3.addEventListener("click", () => {n=10; losses=0; wins=0})
+const rnd4= document.querySelector(".rnd4")
+rnd4.addEventListener("click", () => {n=100; losses=0; wins=0})
+
 const btn1= document.querySelector(".btn1")
-btn1.addEventListener("click", () => {playerSelection = btn1.textContent; play(playerSelection)})
+btn1.addEventListener("click", () => {
+    playerSelection = "Hammer"; 
+    document.querySelector(".final").textContent = ""
+    play(playerSelection)
+})
+
 const btn2= document.querySelector(".btn2")
-btn2.addEventListener("click", () => {playerSelection = btn2.textContent; play(playerSelection)})
+btn2.addEventListener("click", () => {
+    playerSelection = "Sheild";
+    document.querySelector(".final").textContent = "" 
+    play(playerSelection)
+})
+
 const btn3= document.querySelector(".btn3")
-btn3.addEventListener("click", () => {playerSelection = btn3.textContent; play(playerSelection)})
+btn3.addEventListener("click", () => {
+    playerSelection = "Bow"; 
+    document.querySelector(".final").textContent = ""
+    play(playerSelection)
+})
 
 function play(playerSelection) {
     computerSelection = computerPlay()
@@ -19,47 +43,49 @@ function play(playerSelection) {
     }else {
         document.querySelector(".result").textContent = result + " " + playerSelection + " ties " + computerSelection
     }
-    document.querySelector(".win").textContent = "wins:" + wins 
-    document.querySelector(".loss").textContent = "losses:" + losses
+    document.querySelector(".win").textContent = "Player:" + wins 
+    document.querySelector(".loss").textContent = "Computer:" + losses
 
-    if(wins == 5){
-        alert("player wins")
+    if(wins == n){
+        document.querySelector(".final").setAttribute("style","color:gold")
+        document.querySelector(".final").textContent = "Player Wins"
         wins = 0
         losses = 0
-    }else if(losses == 5){
-        alert("cpu wins")
+    }else if(losses == n){
+        document.querySelector(".final").setAttribute("style","color:purple")
+        document.querySelector(".final").textContent = "Computer Wins"
         wins = 0
         losses = 0
     }
 }
 
 function computerPlay() {
-    const play=["rock", "paper", "scissors"]
+    const play=["Hammer", "Bow", "Sheild"]
     return play[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
     let result
-    if(playerSelection == "rock") {
-        if(computerSelection == "rock") {
+    if(playerSelection == "Hammer") {
+        if(computerSelection == "Hammer") {
             result = "tie!"
-        }else if(computerSelection == "paper") {
+        }else if(computerSelection == "Bow") {
             result = "you lose!"
         }else {
             result = "you win!"
         }
-    }else if(playerSelection == "paper") {
-        if(computerSelection == "rock") {
+    }else if(playerSelection == "Bow") {
+        if(computerSelection == "Hammer") {
             result = "you win!"
-        }else if(computerSelection == "paper") {
+        }else if(computerSelection == "Bow") {
             result = "tie!"
         }else {
             result = "you lose!"
         }
     }else {
-        if(computerSelection == "rock") {
+        if(computerSelection == "Hammer") {
             result = "you lose!"
-        }else if(computerSelection == "paper") {
+        }else if(computerSelection == "Bow") {
             result = "you win!"
         }else {
             result = "tie!"
